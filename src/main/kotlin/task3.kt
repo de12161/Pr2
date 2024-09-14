c20570e44f8ced9c73931f80e7ddd36581320a3d
de12161 <162108835+de12161@users.noreply.github.com>
2024-09-14 17:08:57 +0300

    Finish task3.kt

---
+++ src/main/kotlin/task3.kt
@@ -0,0 +1,68 @@
+fun verifyStr(str:String, alphabet:Array<Char>): Boolean {
+    for (char in str) {
+        if (!alphabet.contains(char)) return false
+    }
+
+    return true
+}
+
+fun getStr(msg:String, alphabet: Array<Char>): String {
+    var ret:String
+
+    while (true) {
+        print(msg)
+        ret = readln()
+
+        if (!verifyStr(ret, alphabet)) {
+            println("Строка должна содержать только строчные буквы русского алфавита")
+            continue
+        }
+
+        return ret
+    }
+}
+
+fun myMod(x:Int, y:Int): Int {
+    return (y + x % y) % y
+}
+
+fun encode(str:String, key:String, alphabet: Array<Char>): String {
+    var ret = ""
+
+    for (i in str.indices) {
+        ret += alphabet[myMod(alphabet.indexOf(str[i]) + key[myMod(i, key.length)].code, alphabet.size)]
+    }
+
+    return ret
+}
+
+fun decode(str:String, key:String, alphabet: Array<Char>): String {
+    var ret = ""
+
+    for (i in str.indices) {
+        ret += alphabet[myMod(alphabet.indexOf(str[i]) - key[myMod(i, key.length)].code, alphabet.size)]
+    }
+
+    return ret
+}
+
+fun main() {
+    val alphabet:Array<Char> = Array(32) { (it + 1072).toChar() }
+
+    val mode:Int = getInt("Введите 1 для шифрования текста или 0 для расшифровки: ", 0, 1)
+
+    val key:String
+    val input:String
+
+    if (mode == 1) {
+        key = getStr("Введите ключевое слово: ", alphabet)
+        input = getStr("Введите слово для шифрования: ", alphabet)
+
+        println("Зашифрованный текст: ${encode(input, key, alphabet)}")
+    } else if (mode == 0) {
+        key = getStr("Введите ключевое слово: ", alphabet)
+        input = getStr("Введите слово для расшифровки: ", alphabet)
+
+        println("Расшифрованный текст: ${decode(input, key, alphabet)}")
+    }
+}
